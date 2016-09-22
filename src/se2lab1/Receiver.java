872a698abaf0c21f052740d0c2a5303d31647b2f
Amer <Amer@10.235.31.136>
2016-09-22 18:50:26 +0200

    recive object from server not just from file

--- src/se2lab1/Receiver.java
+++ src/se2lab1/Receiver.java
@@ -1,30 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package se2lab1;
 
-/**
- *
- * @author amharb
- */
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
-import se2lab1.ThePanel;
+import java.io.PrintStream;
+import java.net.Socket;
 
-public class Receiver {
+public class Receiver
+{
 
-    public void receive() {
+    //in case the file is taken from server
+    private final int PORT = 8000;
+    private final String SERVER = "127.0.0.1";
+    private final boolean FROM_FILE = false;
+
+    public void receive()
+    {
         String filename = "../SE2Lab1Sender/panel.ser";
         TheFrame theFrame = null;
         ThePanel panel = null;
         FileInputStream fis = null;
         ObjectInputStream in = null;
         try {
-            fis = new FileInputStream(filename);
-            in = new ObjectInputStream(fis);
+
+            if (FROM_FILE) {// the file case TODO later server
+                fis = new FileInputStream(filename);
+                in = new ObjectInputStream(fis);
+            } else { //from network
+                Socket socket = new Socket(SERVER, PORT);
+                InputStream is = socket.getInputStream();
+                //isr = new InputStreamReader(is);
+                //STOP BR br = new BufferedReader(isr);
+                in = new ObjectInputStream(is);
+
+            }
             panel = (ThePanel) in.readObject();
             in.close();
         } catch (IOException ex) {
